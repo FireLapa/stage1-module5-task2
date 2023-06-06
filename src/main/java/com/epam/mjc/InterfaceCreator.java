23c729dc118a8ac946760b0494a74511d0d3eaed
Lapa <lapabig@gmail.com>
2023-06-06 17:09:46 +0600

    Updated

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -6,12 +6,16 @@ import java.util.List;
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
+
         return (List<Integer> arg) -> {
             List<Integer> result = new ArrayList<>();
+
             for (Integer number : arg) {
                 result.add(number / divider);
             }
+
             return result;
         };
+
     }
 }
